bbe9089b2e97822763260487654c7ca9df7838e8
helmiket <hescamilla@itexico.net>
2017-11-01 15:12:30 -0600

    Include click event

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-var app, app2;
+var apps = [];
 
 
 function init() {
-    app = new Vue({
+    apps.push(new Vue({
         el: '#app',
         data: {
             message: 'Hello Panchito!'
         }
-    });
+    }));
 
-    app2 = new Vue({
+    apps.push(new Vue({
         el: '#app-2',
         data: {
             message: 'You loaded this page on:' + new Date()
         }
-    });
+    }));
+
+    apps.push(new Vue({
+        el: '#app-3',
+        data: {
+            seen: true
+        }
+    }));
+
+    apps.push(new Vue({
+        el: '#app-4',
+        data: {
+            todos: [
+                {text: 'Hello'},
+                {text: 'World'},
+                {text: '!'}
+            ]
+        }
+    }));
+
+    apps.push(new Vue({
+        el: '#app-5',
+        data: {
+            message: 'Hello Vue.js'
+        },
+        methods: {
+            reverseMessage: function() {
+                this.message = this.message.split('').reverse().join('')
+            }
+        }
+    }));
 }
